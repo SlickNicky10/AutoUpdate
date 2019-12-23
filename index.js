5291fd64a38d5c6350d0aa283b6e92244f3c2a01
SlickNicky10 <slicknicky10@gmail.com>
2019-12-23 00:04:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = () => {
     for(var i in modules){
         const module = modules[i];
         if(module.isDirectory()){
-            const data = JSON.parse(manager.readFile(new java.io.File(`${module.getPath()}\\package.json`)));
+            const data = JSON.parse(manager.readFile(new java.io.File(`${module.getPath()}/package.json`)));
             if(!data.name || !data.version || !data.repository || !data.repository.url) log(`Module ${module.getName()} has an invalid package.json, and cannot be automatically updated.`);
             else {
                 log(`Found module ${data.name} version ${data.version}`);
